d8ecbddd9ad909faea5574175691a0972aef5ddd
Bruna <bruna.contato26@gmail.com>
2025-01-20 22:07:37 -0300

    exercicio 10

---
+++ Ex010/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, aumento;
+
+            Console.WriteLine("Digite o seu sálario: ");
+
+            salario = double.Parse(Console.ReadLine());
+
+            if (salario > 2500)
+            {
+                aumento = (salario * 0.05) + salario;
+            }
+            else if (salario > 1250)
+            {
+                aumento = (salario * 0.1) + salario;
+            }
+
+            else (salario < 1250)
+            {
+                aumento = salario * 0.15 + salario;
+            }
+
+            Console.WriteLine("O salario final será .");
+
+
+
+
+
+        }
+    }
+}
