3419193ee0095d521f5bdfa65ffc00afc2280971
Bruna <bruna.contato26@gmail.com>
2025-01-22 21:58:29 -0300

    Exercicio 015

---
+++ Ex015/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+
+            Console.Write("Digite o gênero: ");
+            genero = Console.ReadLine().ToUpper();
+
+            while (genero != "F" && genero != "M")
+            {
+                Console.Write("Digte o genêro: ");
+                genero = Console.ReadLine().ToUpper();
+            }
+           
+            Console.WriteLine("Bem-vindos e bem-vindas ao curso de C#!");
+        }   
+            
+    }
+}
