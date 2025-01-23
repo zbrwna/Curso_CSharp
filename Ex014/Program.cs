22c38d004860327eefd9ab7b507d7f8b65476894
Bruna <bruna.contato26@gmail.com>
2025-01-22 21:34:59 -0300

    Exercicio tabuada for

---
+++ Ex014/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Digite um numero e descubra sua tabuada");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+            }
+               
+        }
+    }
+}
