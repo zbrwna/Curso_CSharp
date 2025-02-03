77c51899f08d5477d571ddb6a6c68e72010167a2
Bruna <bruna.contato26@gmail.com>
2025-02-03 19:47:35 -0300

    ex027votacao

---
+++ Ex028Votacao/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Ex028Votacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nasc;
+            Console.WriteLine("Digite o ano de nascimento: ");
+            nasc = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(voto(nasc));
+        }
+
+        static string voto(int ano)
+        {
+            DateTime dataAtual = DateTime.Now;
+           /*int dia = dataAtual.Day;
+             int mes = dataAtual.Month;
+             int hora = dataAtual.Hour;
+             int minuto = dataAtual.Minute;
+             int segundo = dataAtual.Second;*/
+            int anoAtual = dataAtual.Year;
+            int idade = anoAtual - ano;
+
+            if (idade < 16)
+            {
+                return $"Com {idade} anos, você não vota!";
+            }
+            else if (idade > 18 && idade < 70)
+            {
+                return $"Com {idade} anos, você vota!";
+            }
+            else
+            {
+                return $"Com {idade} anos, seu voto é opcional!";
+            }
+        }
+    }
+}
