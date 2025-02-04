2eb69ea7db7b8c951c7be4933d687e86626f1efd
Bruna <bruna.contato26@gmail.com>
2025-02-03 21:18:19 -0300

    ex028 Fatorial

---
+++ Ex029Fatorial/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Ex029Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            char resp;
+            bool mostrar = false;
+
+            Console.Write("Digite um número inteiro: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Deseja mostrar os cálculos? (s/n): ");
+            resp = Convert.ToChar(Console.ReadLine().ToLower());
+            Console.WriteLine();
+
+            if (resp == 's') { mostrar = true; }
+
+            Console.WriteLine(fatorial(num, mostrar));
+
+        }
+
+        static int fatorial (int n, bool show = false)
+        {
+            int f = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (show)
+                {
+                    Console.Write(i);
+                    if (i < n)
+                    {
+                        Console.Write(" x ");
+                    }
+                    else
+                    {
+                        Console.Write(" = ");
+                    }
+                }
+
+                f *= i;
+            }
+
+            return f;
+            
+        }
+    }
+}
